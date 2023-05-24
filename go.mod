080c27552826b70e970217aab019f275bcf712cd
null122 <1229608477@qq.com>
2023-05-25 01:34:48 +0800

    init

---
+++ go.mod
@@ -0,0 +1,7 @@
+module protoc-gen-ts-http
+
+go 1.18
+
+require google.golang.org/genproto v0.0.0-20230410155749-daa745c078e1
+
+require google.golang.org/protobuf v1.30.0
---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"google.golang.org/protobuf/proto"
+	"google.golang.org/protobuf/types/pluginpb"
+	"io"
+	"os"
+	"path/filepath"
+	"protoc-gen-ts-http/plugin"
+)
+
+func main() {
+	if err := run(); err != nil {
+		fmt.Fprintf(os.Stderr, "%s: %v\n", filepath.Base(os.Args[0]), err)
+		os.Exit(1)
+	}
+}
+
+func run() error {
+	in, err := io.ReadAll(os.Stdin)
+	if err != nil {
+		return err
+	}
+	req := &pluginpb.CodeGeneratorRequest{}
+	if err := proto.Unmarshal(in, req); err != nil {
+		return err
+	}
+	resp, err := plugin.GenerateFile(req)
+	if err != nil {
+		return err
+	}
+	out, err := proto.Marshal(resp)
+	if err != nil {
+		return err
+	}
+	if _, err := os.Stdout.Write(out); err != nil {
+		return err
+	}
+	return nil
+}
